10a473ccc3408471d8fbc478e0f3f0ed1342626a
KayqueSilveira <48252382+KayqueSilveira@users.noreply.github.com>
2023-08-20 17:45:57 -0300

    Add files via upload

---
+++ RCP/src/test/java/com/grupo/criar/RCP/RcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo.criar.RCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
