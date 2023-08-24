c6bfc4422e735fd5e5cfb4741cc98742982db82a
Kayque Silveira <kayque.silveira@outlook.com>
2023-08-24 02:05:50 -0300

    add repository

--- RCP/src/main/java/com/grupo/criar/RCP/Repository/RaceRepository.java
+++ RCP/src/main/java/com/grupo/criar/RCP/Repository/RaceRepository.java
@@ -3,5 +3,9 @@ package com.grupo.criar.RCP.Repository;
 import com.grupo.criar.RCP.Models.Race;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RaceRepository extends JpaRepository<Race, Long> {
+
+    List<Race> findByPilotName (final String name);
 }
